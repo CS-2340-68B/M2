6a188cc60f5fd08f01592fdc4d9ed44c914aca24
Thinh Nguyen <tnntech@gatech.edu>
2018-09-05 12:46:08 -0400

    new file and delete useless file

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
