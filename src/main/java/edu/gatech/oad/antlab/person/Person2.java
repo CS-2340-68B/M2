a10cd27b063a5c4e3af7ee5913116741bfded1a0
Thinh Nguyen <tnntech@gatech.edu>
2018-09-05 19:34:26 -0400

    restructure person 2 code

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,5 +1,7 @@
 package edu.gatech.oad.antlab.person;
-import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
@@ -30,18 +32,16 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-		int totalLength = input.length();
-		Random generator = new Random();
-		StringBuilder sb = new StringBuilder();
-		char curr = '0';
-		int index = 0;
-		do {
-			index  = generator.nextInt(totalLength);
-			curr = input.charAt(index);
-			sb.append(curr);
-		} while (index != 1);
-
-		return sb.toString();
+		List<Character> characters = new ArrayList<>();
+		for (char c:input.toCharArray()) {
+			characters.add(c);
+		}
+		StringBuilder output = new StringBuilder(input.length());
+		while (characters.size() != 0) {
+			int randPick = (int) (Math.random() * characters.size());
+			output.append(characters.remove(randPick));
+		}
+		return output.toString();
 	}
 	/**
 	 * Return a string rep of this object
---
+++ src/main/java/edu/gatech/oad/antlab/pkg3/AntLab31.java
@@ -0,0 +1,12 @@
+package edu.gatech.oad.antlab.pkg3;
+
+public class AntLab31 {
+
+    /**
+     * retrieves a pre-stored string message
+     * @return the string
+     */
+//    public String getMessage() {
+//        return " a";
+//    }
+}
