9a72eccf54ed6af10ab8a9a4da86c04739008096
Trinh Do <trinhiu21@gmail.com>
2018-09-05 12:30:07 -0400

    change calc

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -31,7 +31,7 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  
-	  return input.substring(0,2) + input.substring(2,input.length());
+	  return  input.substring(2,input.length()) + input.substring(0,2);
 	}
 	
 	/**
