b9caafdf156163be60aac6b9890db801450ce61b
Nhat Lan Le Tu <35468473+nhat-lan@users.noreply.github.com>
2018-09-04 23:54:58 -0400

    Update Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Nhat Lan Le Tu
  *  @version 1.1
  */
 public class Person4 {
