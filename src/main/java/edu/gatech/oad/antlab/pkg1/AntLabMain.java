c254136fa5d084296536269f35f6294aa134461c
Trinh Do <trinhiu21@gmail.com>
2018-09-06 17:09:02 -0400

    change

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,8 +71,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Trinh Do");
+                  toPrint += p5.toString("tdo48");
 		  
         System.out.println(toPrint);
     }
